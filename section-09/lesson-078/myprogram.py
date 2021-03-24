5078b335e3e4c170f9858494a7258e492754ae47
amarchino <marchino.alessandro@gmail.com>
2021-03-24 21:04:17 +0000

    Lezione 78

---
+++ section-09/lesson-078/myprogram.py
@@ -0,0 +1,7 @@
+from mymodule import my_func
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import my_subscript
+
+my_func()
+some_main_script.report_main()
+my_subscript.sub_report()
